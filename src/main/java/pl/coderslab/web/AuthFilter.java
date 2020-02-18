612bd456aaa593f4307a8ceb60f3b126af41e8cf
Wojciech Kuziow <wkuziow@gmail.com>
2020-02-18 22:42:33 +0100

    AuthFilter2 WK

---
+++ src/main/java/pl/coderslab/web/AuthFilter.java
@@ -0,0 +1,37 @@
+package pl.coderslab.web;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter("/app/*")
+public class AuthFilter implements Filter {
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+
+//        HttpServletResponse httpResponse = (HttpServletResponse) resp;
+//        HttpServletRequest httpRequest = (HttpServletRequest) req;
+        HttpSession sess = ((HttpServletRequest) req).getSession();
+        String username = (String) sess.getAttribute("username");
+        if (username == null || username.isEmpty()) {
+            ((HttpServletResponse) resp).sendRedirect("/login");
+            return;
+        }
+
+
+
+
+
+        chain.doFilter(req, resp);
+    }
+
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
+}
